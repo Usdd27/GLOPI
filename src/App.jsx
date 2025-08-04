a983ae815839911b31a7cb637def8eb143e7e038
Usdd27 <usdd7.co2797@gmail.com>
2025-08-04 17:12:01 +0300

    5

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Hero from './components/Hero';
-import Tokenomics from './components/CurrentMemeToken';
-import AirdropGame from './components/MemeNewsLaunchpad';
-import Roadmap from './components/TopMemeTokens';
+import CurrentMemeToken from './components/CurrentMemeToken';
+import MemeNewsLaunchpad from './components/MemeNewsLaunchpad';
+import TopMemeTokens from './components/TopMemeTokens';
 import Socials from './components/Socials';
-import Buy from './components/GrabYourMeme';
+import GrabYourMeme from './components/GrabYourMeme';
 import Team from './components/Team';
 import Footer from './components/Footer';
 
@@ -11,11 +11,11 @@ export default function App() {
   return (
     <>
       <Hero />
-      <Tokenomics />
-      <AirdropGame />
-      <Roadmap /> {/* ✅ компонент с топ мем-токенами */}
+      <CurrentMemeToken />
+      <MemeNewsLaunchpad />
+      <TopMemeTokens /> {/* ✅ компонент с топ мем-токенами */}
       <Socials />
-      <Buy />
+      <GrabYourMeme />
       <Team />
       <Footer />
     </>
